f530ba49dc2039126f144e39fde9cfc7bf6a42ec
rob.tisdale <rtisdale.bt@gmail.com>
2023-03-31 17:43:46 -0500

    init boot project

---
+++ DIYProject/src/main/java/com/skilldistillery/diy/DiyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.diy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiyProjectApplication.class, args);
+	}
+
+}
---
+++ DIYProject/src/test/java/com/skilldistillery/diy/DiyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.diy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
